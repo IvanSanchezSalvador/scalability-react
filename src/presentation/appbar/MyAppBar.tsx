1c7777db25f921577f93873dab4a14e047741715
sanchiv <isanchez@intrasoft-intl.com>
2021-07-27 15:35:44 +0200

    Initial commit

---
+++ src/presentation/appbar/MyAppBar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { useCartBloc } from "../app/App";
+import { UpdatedCartState, CartState } from "../cart/CartState";
+import { BlocBuilder } from "../common/bloc";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  toolbar: {
+    justifyContent: "space-between",
+    maxWidth: "800",
+  },
+}));
+
+const MyAppBar: React.FC = () => {
+  const classes = useStyles();
+  const bloc = useCartBloc();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <IconButton color="inherit">
+          <BlocBuilder
+            bloc={bloc}
+            builder={(state: CartState) => {
+              const totalItems =
+                bloc.state.kind === "UpdatedCartState"
+                  ? (bloc.state as UpdatedCartState).totalItems
+                  : 0;
+
+              return (
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCartIcon onClick={() => bloc.openCart()} />
+                </Badge>
+              );
+            }}
+          />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MyAppBar;
